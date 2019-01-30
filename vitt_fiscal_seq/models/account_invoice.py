705ed1d052bbba7670d89dbdec72a17dcb676f5e
calix <darwin.calix@d2i-solutions.com>
2019-01-30 10:39:27 -0600

    Fecha de Expiracion update

--- vitt_fiscal_seq/models/account_invoice.py
+++ vitt_fiscal_seq/models/account_invoice.py
@@ -320,7 +320,7 @@ class AccountInvoice(models.Model):
         res = super(AccountInvoice, self).action_date_assign()
         today = datetime.now()
         if self.sequence_ids:
-            if today > datetime.strptime(self.sequence_ids.expiration_date ,'%Y-%m-%d'):
+            if today > self.sequence_ids.expiration_date:
                 raise Warning(_('The Expiration Date for this fiscal sequence is %s ') % (self.sequence_ids.expiration_date))
             if self.sequence_ids.vitt_number_next_actual > self.sequence_ids.max_value:
                 raise Warning(_('The range of sequence numbers is finished'))
