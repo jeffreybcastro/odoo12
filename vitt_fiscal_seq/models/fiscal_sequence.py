cad1192638bda28555f1cfb8a9a01042bbca191e
calix <darwin.calix@d2i-solutions.com>
2019-02-01 07:11:07 -0600

    [UPDATE] Example

--- vitt_fiscal_seq/models/fiscal_sequence.py
+++ vitt_fiscal_seq/models/fiscal_sequence.py
@@ -1,7 +1,6 @@
 # -*- coding: utf-8 -*-
 ##############################################################################
 # For copyright and license notices, see __manifest__.py file in module root
-# directory
 ##############################################################################
 
 from odoo import models, fields, api, _
