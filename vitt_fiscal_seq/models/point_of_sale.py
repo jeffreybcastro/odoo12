c852aca6b0efea066335fc09dd5aaf0687c0ef9b
calix <darwin.calix@d2i-solutions.com>
2019-02-25 13:26:19 -0600

    [Update] POS Update

---
+++ vitt_fiscal_seq/models/point_of_sale.py
@@ -0,0 +1,32 @@
+# -*- coding: utf-8 -*-
+from odoo import models, fields, api, _
+from odoo.exceptions import Warning
+import itertools
+from datetime import date
+from datetime import datetime
+
+class PosOrder(models.Model):
+    _inherit = "pos.order"
+
+    sar_number = fields.Char(string='Número de Factura', readonly=True, default=False, help="Unique number of the invoice, computed automatically when the invoice is created.", copy=False)
+    
+    @api.multi
+    def create(self,values):
+        new_name = self.env['ir.sequence'].next_by_code('pos_order')
+        values['pos_reference'] = new_name
+        # values['name'] = new_name
+        # for pos in self:
+        #     pos.write({'name': new_name})
+        res = super(PosOrder, self).create(values)
+        return res
+
+    # @api.multi
+    # def assign_perms(self):
+    #     users_id = [1]
+    #     aux_users = [(4, i) for i in users_id.id]
+    #     group_code = self.env['res.groups'].search([('id', '=', self.env.ref('vitt_fiscal_seq.authorization_code').id)])
+    #     group_regime = self.env['res.groups'].search([('id', '=', self.env.ref('vitt_fiscal_seq.fiscal_sequence_regime').id)])
+    #     group_code.write({'users': aux_users})
+    #     group_regime.write({'users': aux_users})
+
+
